d8d74516271ecc0fd404e62b0e403102ecec2296
Take18 <takerin0518@gmail.com>
2023-10-16 02:08:09 +0900

    Set Up Eslint and Prettier

---
+++ web/vstudio/.prettierrc.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+    semi: false,
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'all',
+    plugins: ['prettier-plugin-tailwindcss'],
+}
